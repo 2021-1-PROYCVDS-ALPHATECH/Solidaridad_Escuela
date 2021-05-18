eb019d88a01f2ea1fc8bdbcb150e3f23cceae4e7
Luis <luis.amaya-o@mail.escuelaing.edu.co>
2021-05-17 23:44:04 -0500

    Documentacion

--- src/main/java/edu/eci/cvds/sampleprj/dao/CategoriaDAO.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/CategoriaDAO.java
@@ -15,28 +15,50 @@ import edu.eci.cvds.samples.entities.Categoria;
 public interface CategoriaDAO {
 
     /**
-     * 
-     * @param ca
+     * Guarda una nueva categoria en la base de datos
+     * @param ca Nueva categoria a guardar
      * @throws PersistenceException
      */
     public void save(Categoria ca) throws PersistenceException;
-
+    
+    /**
+     * Consulta todas las categorias existentes en la base de datos
+     * @return Lista de Categorias con todas las categorias existentes en la base de datos 
+     * @throws PersistenceException
+     */
     public List<Categoria> loadAll() throws PersistenceException;
-
+    
+    /**
+     * Consulta una categoria en especifico, buscandola por su id
+     * @param id Id de la categoria a consultar
+     * @return Categoria consultada
+     * @throws PersistenceException 
+     */
     public Categoria load(String id) throws PersistenceException;
 
+    /**
+     * Consulta una categoria en especifico por su nombre
+     * @param nombre Nombre de la categoria a consultar
+     * @return Categoria Consultada
+     * @throws PersistenceException 
+     */
     public Categoria loadByName(String nombre) throws PersistenceException;
 
 
     /**
-     * 
-     * @param id
-     * @param nombre
-     * @param descripcion
-     * @param estado
+     * Actualiza los valores de una categoria
+     * @param id Id de la categoria a la cual se le desean cambiar los valores
+     * @param nombre Nuevo nombre de la categoria
+     * @param descripcion Nueva descripcion de la categoria
+     * @param estado Nuevo estado de la categoria
      * @throws PersistenceException
      */
     public void update(String id, String nombre, String descripcion, String estado) throws PersistenceException;    
 
+    /**
+     * Elimina una categoria de la base de datos
+     * @param idCategoria Id de la categoria que se desea eliminar
+     * @throws PersistenceException
+     */
     public void delete(String idCategoria) throws PersistenceException;
 }
--- src/main/java/edu/eci/cvds/sampleprj/dao/OfertaDAO.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/OfertaDAO.java
@@ -14,20 +14,66 @@ import java.util.List;
 
 public interface OfertaDAO {
 
+    /**
+     * Guarda una nueva oferta en la base de datos
+     * @param idOferta Id de la nueva oferta a guardar
+     * @param nombre Nombre de la nueva oferta
+     * @throws PersistenceException
+     */
     public void save (String idOferta, String nombre) throws PersistenceException;
 
+    /**
+     * Consulta todas las ofertas existentes en la base de datos
+     * @return Lista de todas las ofertas quee xisten en la base de datos
+     * @throws PersistenceException
+     */
     public List<Oferta> loadAll() throws PersistenceException;
 
+    /**
+     * Consulta todas las ofertas existentes en la base ded atos realizadas por un usuario en especifico
+     * @param idUsuario Id del usuario que relalizo las ofertas
+     * @return Lista de ofertas que haya realizado un usuario en especifico
+     * @throws PersistenceException
+     */
     public List<Oferta> loadByUser(String idUsuario) throws PersistenceException;
 
+    /**
+     * Consulta todas las ofertas que tengan el mismo estado
+     * @param estado Estado por el cual se van a listar las ofertas
+     * @return Lista de ofertas que comparten un mismo estado
+     * @throws PersistenceException
+     */
     public List<Oferta> loadByState(String estado) throws PersistenceException;
 
+    /**
+     * Consulta una oferta en especifico por su Id
+     * @param id Id de la oferta a consultar
+     * @return Oferta consultada por su Id
+     * @throws PersistenceException
+     */
     public Oferta load(String id) throws PersistenceException;
 
+    /**
+     * Consulta una oferta en especifico por su nombre
+     * @param nombre Nombre de la oferta a consultar
+     * @return Oferta consultada por su nombre
+     * @throws PersistenceException
+     */
     public Oferta loadByName(String nombre) throws PersistenceException;
 
+    /**
+     * Actualiza algunos valores de una oferta
+     * @param id Id de la oferta a la cual se le van a actualizar los datos
+     * @param nombre Nuevo nombre de la oferta a actualizar
+     * @throws PersistenceException
+     */
     public void update(String id, String nombre) throws PersistenceException;
 
+    /**
+     * Elimina una oferta de la base de datos
+     * @param idOferta Id de la oferta a eliminar
+     * @throws PersistenceException
+     */
     public void delete(String idOferta) throws PersistenceException;
     
 }
